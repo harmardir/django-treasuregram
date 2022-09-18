41921d421216e99f9d8b46f2c7a3d656177485c6
Harout <harmardir@gmail.com>
2022-09-18 11:07:58 +0300

    using class to pass more data in dictionary context

--- treasuregram/main_app/views.py
+++ treasuregram/main_app/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    name = 'Gold Nugget'
-    value = 1000.00
-    context= {
-        'treasure_name':name,
-        'treasure_val':value
-    }
-    return render(request , 'index.html',context)
+    return render(request , 'index.html',{'treasures': treasures})
+
+
+class Treasure:
+    def __init__(self, name, value , material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self .location = location
+
+treasures = [
+    Treasure("Gold Nugget", 500.00, "gold", "Cutly's Creek, NM"),
+    Treasure("Fool's Gold", 0, "pyrite", "Fools Falls, CO"),
+    Treasure("Coffee Can", 20.00, "tin", "Acme, CA ")
+]
