7505052985f6130a34fc236d15ad53f58fa45927
Harout <harmardir@gmail.com>
2022-09-18 18:40:57 +0300

    styling template and adding static files

--- treasuregram/main_app/views.py
+++ treasuregram/main_app/views.py
@@ -6,11 +6,12 @@ def index(request):
 
 
 class Treasure:
-    def __init__(self, name, value , material, location):
+    def __init__(self, name, value , material, location, img_url):
         self.name = name
         self.value = value
         self.material = material
         self .location = location
+        self.img_url = img_url
 
 treasures = [
     Treasure("Gold Nugget", 500.00, "gold", "Cutly's Creek, NM"),
